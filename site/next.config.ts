1fee1ff08d9c3eb9904c5bdc55d95b92b3409359
kel <kyle@intelvis.ai>
2025-07-09 19:32:43 -0700

    wiping github pages

--- site/next.config.ts
+++ site/next.config.ts
@@ -6,8 +6,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/site' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/site' : '',
 };
 
 export default nextConfig;
